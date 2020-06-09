d7d6d88a6ed674957e09de5301d320530ad00c63
immxalan <aedocoding@gmail.com>
2020-06-09 02:16:27 -0700

    Removed unused code references, finished queue with linked lists

--- queue/queue.py
+++ queue/queue.py
@@ -13,16 +13,57 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage =[]
+    
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def enqueue(self, value):
+#         return self.storage.insert(0, value)
+
+#     def dequeue(self):
+#         return self.storage.pop() if self.storage else None
+
+class Node: 
+    # Function to initialise the node object 
+    def __init__(self, data): 
+        self.data = data # Assign data 
+        self.next = None # Initialize next as null 
+class LinkedList: 
+  
+    # Function to initialize head 
+    def __init__(self): 
+        self.head = None
+        self.tail = None
+  
 class Queue:
     def __init__(self):
-        self.size = 0
-        # self.storage = ?
-    
-    def __len__(self):
-        pass
-
-    def enqueue(self, value):
-        pass
-
+        self.head = None
+        self.tail = None
+    def __len__(self): 
+        temp = self.head # Initialise temp 
+        count = 0 # Initialise count 
+  
+        # Loop while end of linked list is not reached 
+        while (temp): 
+            count += 1
+            temp = temp.next
+        return count 
+    def enqueue(self, data):
+        if self.tail is None:
+            self.head = Node(data)
+            self.tail = self.head
+        else:
+            self.tail.next = Node(data)
+            self.tail = self.tail.next
+ 
     def dequeue(self):
-        pass
+        if self.head is None:
+            return None
+        else:
+            to_return = self.head.data
+            self.head = self.head.next
+            return to_return
--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,62 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def push(self, value):
+#         return self.storage.append(value)
+
+#     def pop(self):
+#         return self.storage.pop() if self.storage else None
+
+# Node class 
+class Node: 
+    # Function to initialise the node object 
+    def __init__(self, data): 
+        self.data = data # Assign data 
+        self.next = None # Initialize next as null 
+  
+  
+# Linked List class contains a Node object 
+class LinkedList: 
+  
+    # Function to initialize head 
+    def __init__(self): 
+        self.head = None
+
 class Stack:
     def __init__(self):
-        self.size = 0
-        # self.storage = ?
-
-    def __len__(self):
-        pass
-
-    def push(self, value):
-        pass
-
+        self.head = None
+ 
+    def push(self, data):
+        if self.head is None:
+            self.head = Node(data)
+        else:
+            new_node = Node(data)
+            new_node.next = self.head
+            self.head = new_node
+ 
     def pop(self):
-        pass
+        if self.head is None:
+            return None
+        else:
+            popped = self.head.data
+            self.head = self.head.next
+            return popped
+        # This function counts number of nodes in Linked List 
+    # iteratively, given 'node' as starting node. 
+    def __len__(self): 
+        temp = self.head # Initialise temp 
+        count = 0 # Initialise count 
+  
+        # Loop while end of linked list is not reached 
+        while (temp): 
+            count += 1
+            temp = temp.next
+        return count 
